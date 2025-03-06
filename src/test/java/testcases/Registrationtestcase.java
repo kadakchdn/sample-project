564f20a025e535e137ea394ca4a028c03b293ac3
kadakchdn <kambleakshay163@gmail.com>
2025-03-06 16:37:57 +0530

    first commit registrationtestcase

---
+++ src/test/java/testcases/Registrationtestcase.java
@@ -0,0 +1,15 @@
+package testcases;
+
+import org.testng.annotations.Test;
+
+public class Registrationtestcase extends Basetestcase
+{
+
+	
+	@Test
+	void Registration()
+	{
+		System.out.println("I am login Test");
+	}
+	
+}
